8026f50c8fce53365f4439f84fed271d9a1b419a
anett-resendiz <anett.resendiz@alumno.buap.mx>
2024-12-08 00:33:17 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,12 +4,12 @@
 // var
 // let nombreDeAlumno;
 // console.log(nombre);
-// nombre = "Anel";
+// nombre = "Anett";
 
 // Hola Mundo desde la consola
-// console.log("Hola Mundo desde la consola!")
+// console.log("Hola Mundo desde la consola!!!")
 // Hola mundo desde un alert
-// alert("hola mundo desde un alert!");
+// alert("hola mundo desde un alert!!!");
 // // tipos de datos
 // // string
 // let texto = "soy un texto";
@@ -25,9 +25,9 @@
 // Definir mis constantes y mis variables
 // ''
 // Código JS
+// Variables y constantes
 const fecha = document.querySelector('#fecha');
 const lista = document.querySelector('#lista');
-const elemento = document.querySelector('#elemento');
 const input = document.querySelector('#input');
 const botonAgregar = document.querySelector('#botonAgregar');
 const check = 'bi-record-circle';
@@ -36,42 +36,58 @@ const uncheck = 'bi-circle';
 let LIST;
 let id;
 
+// Mostrar la fecha actual
 const FECHA = new Date();
 fecha.innerHTML = FECHA.toLocaleDateString('es-MX', {
     weekday: 'long',
     month: 'short',
     day: 'numeric',
-
 });
 
+// Agregar tarea
 function agregarTarea(tarea, id, hecho, eliminar) {
-    if (eliminar) {
-        return
-    };
+    if (eliminar) return;
+
     const realizado = hecho ? check : uncheck;
     const LINE = hecho ? tachado : '';
-    const elemento = ` <li id="elemento">
-    <i id="${id}" data="hecho" class="bi ${realizado}"></i>
-    <p class="tarea-lista text ${LINE}">${tarea}</p>
-    <i id="${id}" data="eliminar" class="bi bi-x"></i>
-</li> `
+    const elemento = `
+    <li id="elemento">
+        <i id="${id}" data="hecho" class="bi ${realizado}"></i>
+        <p class="tarea-lista text ${LINE}">${tarea}</p>
+        <i id="${id}" data="eliminar" class="bi bi-x"></i>
+    </li>`;
     lista.insertAdjacentHTML("beforeend", elemento);
-};
+}
 
+// Marcar tarea como realizada
 function tareaRealizada(element) {
-    element.classlist.toggle(check);
-    element.classlist.toggle(uncheck);
-    element.parentNode.querySelector('.text').classlist.toggle(tachado);
-    LIST[element.id].realizado = LIST[element.id].realizado ?false :true;
-};
+    element.classList.toggle(check);
+    element.classList.toggle(uncheck);
+    const textElement = element.parentNode.querySelector('.text');
+    if (textElement) {
+        textElement.classList.toggle(tachado);
+    } else {
+        console.error('No se encontró el elemento con clase .text');
+    }
+    if (LIST[element.id]) {
+        LIST[element.id].realizado = !LIST[element.id].realizado;
+    }
+}
+
+// Eliminar tarea
 function tareaEliminada(element) {
-    element.parentNode.parentNode.removeChild(element.parentNode);
-    LIST[element.id].eliminar = true;
-};
+    const parent = element.parentNode;
+    parent.parentNode.removeChild(parent);
+    if (LIST[element.id]) {
+        LIST[element.id].eliminar = true;
+    }
+}
+
+// Agregar tareas al hacer clic
 botonAgregar.addEventListener("click", () => {
     const tarea = input.value;
     if (tarea) {
-        agregarTarea(tarea, id, false, false)
+        agregarTarea(tarea, id, false, false);
         LIST.push({
             nombre: tarea,
             id: id,
@@ -83,17 +99,22 @@ botonAgregar.addEventListener("click", () => {
         input.value = "";
     }
 });
-lista.addEventListener("click", function (event) {
+
+// Manejar eventos de lista
+lista.addEventListener("click", (event) => {
     const element = event.target;
-    const elementData = element.attributes.data.value;
-    if (elementData == "hecho") {
+    const elementData = element.attributes.data ? element.attributes.data.value : null;
+    if (!elementData) return;
+
+    if (elementData === "hecho") {
         tareaRealizada(element);
-    } else if (elementData == "eliminar") {
+    } else if (elementData === "eliminar") {
         tareaEliminada(element);
-    };
+    }
     localStorage.setItem("TODO", JSON.stringify(LIST));
 });
 
+// Cargar lista desde localStorage
 let data = localStorage.getItem("TODO");
 if (data) {
     LIST = JSON.parse(data);
@@ -102,12 +123,11 @@ if (data) {
 } else {
     LIST = [];
     id = 0;
-};
+}
 
+// Cargar tareas existentes
 function cargarLista(array) {
-    array.forEach(
-        function (item) {
-            agregarTarea(item.nombre, item.id, item.hecho, item.eliminar);
-        }
-    );
-};
+    array.forEach(item => {
+        agregarTarea(item.nombre, item.id, item.hecho, item.eliminar);
+    });
+}
